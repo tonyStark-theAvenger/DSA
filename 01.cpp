c878d58d5ca838d7f30ec8a09050eb56f6b2f6f1
Vaibhav Bagate <bagatevaibhav555@gmai.com>
2026-01-02 16:50:58 +0530

    Added basic template,input and output file

---
+++ 01.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+void solve()
+{
+    cout << "vaibhav" << endl;
+}
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+    int test;
+    cin >> test;
+
+    for (int t = 1; t <= test; t++)
+    {
+        solve();
+    }
+
+    return 0;
+}
